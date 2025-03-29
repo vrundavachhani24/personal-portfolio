1b7cb2049628d66007849b72dee22f01764c09f4
vrunda <vachhanivrunda24@gmail.com>
2025-03-29 16:11:26 +0530

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router"
 import Header from "./Header"
+import About from "./About"
+import Skills from "./Skills"
+import Projects from "./Projects"
+import Contact from "./Contact"
+import Footer from "./Footer"
 
 const Home = () => {
     return (
@@ -20,6 +25,11 @@ const Home = () => {
                     </a>
                 </div>
             </div>
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+            <Footer />
         </div>
     )
 }
