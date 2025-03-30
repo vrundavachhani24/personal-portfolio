e948aed4a48df6fe2e6e12e127ba70522e2bb52e
vrunda <vachhanivrunda24@gmail.com>
2025-03-30 14:30:37 +0530

    update resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <p className="min-[500px]:text-6xl text-3xl font-bold mt-3 fade-up delay-2">Vrunda Vachhani</p>
                 <p className="text-4xl font-bold mt-3 fade-up delay-3">I'm <span className="text-5xl max-xl:text-4xl bg-gradient-to-r from-blue-900 via-indigo-800 to-purple-800 bg-clip-text text-transparent"> Developer</span></p>
                 <div className="mt-5 fade-up delay-4">
-                    <a href="vrundavachhani.pdf" download='VRUNDA_VACHHANI'>
+                    <a href="vrunda Resume.pdf" download='VRUNDA_VACHHANI'>
                         <button type="submit" className=" flex gap-3 p-4 cursor-pointer bg-blue-950 text-white font-bold rounded-4xl bg-linear-to-t from-sky-700 to-indigo-900 hover:bg-black hover:bg-none max-sm:w-50">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
